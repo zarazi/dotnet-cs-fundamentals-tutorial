e32a881531cc40c5b2cbba37fc7ee3414f4e4c46
zarazi <zarazi@hotmail.com>
2020-04-11 11:34:37 +0700

    M4 done

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
+
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("PK+'s Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
+        }
+    }
+}
